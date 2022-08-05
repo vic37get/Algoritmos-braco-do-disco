0f623f789177dd75a8564a2d2b1c8d33ace840d0
Victor <victorribeiro172@gmail.com>
2022-08-05 20:06:10 -0300

    criação do FCFS

---
+++ Braço do disco.py
@@ -0,0 +1,24 @@
+import sys
+
+arquivo = sys.stdin
+dados = arquivo.readlines()
+
+for index in range(0, len(dados)):
+    dados[index] = int(dados[index])
+
+qtd_cilindros = dados[0]
+inicio = dados[1]
+requisicoes = dados[2:]
+
+def FCFS():
+    movimentacoes = 0
+    anterior = inicio
+    for indice, req in enumerate(requisicoes):
+        if req > qtd_cilindros:
+            print('Maior que o número de cilindros!')
+        else:
+            movimentacoes = movimentacoes + (abs(req - anterior))
+            anterior = req
+    return movimentacoes
+    
+print('FCFS: {}'.format(FCFS()))
